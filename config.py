1da944a8c5635b808fe959463d691d975eb0296e
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-04-25 01:34:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 22434619
+    API_HASH = "8390c5ed36dd0527e728089b04b6d9be"
+    TOKEN = "6289589949:AAFkSNs-ioCJKKSICKhibnAwpSEWEujykuE"  
+    START_PIC = "https://te.legra.ph/file/828153bc9f76211e4152e.jpg" 
+    CHAT = "-1001615276723"    
